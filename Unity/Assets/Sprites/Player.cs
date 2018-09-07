88e731ac9d4f2929631837c0746ac5d8edc4db54
vantos3001 <iw.lasgin2011@gmail.com>
2018-09-07 22:33:27 +0300

    add player

---
+++ Unity/Assets/Sprites/Player.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+	[SerializeField]
+	private GameObject _player;
+
+	private Transform _target;
+	
+	[SerializeField]
+	private float _speed;
+	
+	private int _health;
+	private int _damage;
+	// Use this for initialization
+	void Start () {
+		if (_player != null)
+		{
+			_target = _player.GetComponent<Transform>();
+		}
+		else
+		{
+			Debug.LogError("Player is null");
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Move();
+	}
+	
+	private void DealDamage(){
+	}
+		
+	private void Move(){
+		if (Input.GetKey(KeyCode.A)){
+			_target.position -= _speed * new Vector3(1, 0, 0);
+		}
+		if (Input.GetKey(KeyCode.D)){
+			_target.position += _speed * new Vector3(1, 0, 0);
+		}
+		
+		if (Input.GetKey(KeyCode.W)){
+			_target.position += _speed * new Vector3(0, 1, 0);
+		}
+		
+		if (Input.GetKey(KeyCode.S)){
+			_target.position -= _speed * new Vector3(0, 1, 0);
+		}
+	}
+	
+}
